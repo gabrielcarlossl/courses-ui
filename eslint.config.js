c7f72865b520daa30cf39c1230b132c6a7368afc
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-09-17 10:14:05 -0300

    changed app name

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'semi': ['error', 'never'],
+      'semi': ['error', 'never']
     },
   },
 )
