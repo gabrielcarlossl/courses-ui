d557e69f52a216163bcc5f657c6a30e9c74f33bd
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-09-17 10:14:12 -0300

    added styles constants

---
+++ src/utils/StyleConstants.tsx
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react'
+import { createTheme } from '@mui/material'
+
+export const Theme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+})
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+export const ChakraTheme = extendTheme({ colors })
